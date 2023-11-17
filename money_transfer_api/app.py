786923ac67dd8b625735d087f000e0c156027c58
Cian-McKelvey <cian.p.mckelvey@gmail.com>
2023-11-17 22:53:29 +0000

    Adding the start of user authentication to make a better login

--- money_transfer_api/app.py
+++ money_transfer_api/app.py
@@ -1,17 +1,83 @@
 from flask import Flask, request, make_response, jsonify, render_template
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
 from database_methods import add_user, user_exists, delete_user, update_users_password
 from database_methods import update_user_balance, add_transfer, unique_email_check
 import datetime
 import uuid
+import jwt
+from jwt import ExpiredSignatureError, InvalidTokenError
+from functools import wraps
+from constants import SECRET_KEY, CLIENT_CONNECTION, DATABASE_CONNECTION, USERS_COLLECTION
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+
+# Decorator function used to protect routes
+# Should probably add this to all the routes
+def jwt_required(func):
+    @wraps(func)
+    def jwt_required_wrapper(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'message': 'Token is missing'}, 401)
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired'}, 401)
+        except InvalidTokenError:
+            return jsonify({'message': 'Invalid token'}, 401)
+        except Exception as e:
+            return jsonify({'message': f'Error decoding token: {str(e)}'}, 401)
+        return func(*args, **kwargs)
+    return jwt_required_wrapper()
 
 
 # Home route of app
 @app.route('/')
 def hello_world():
     return "<h1>Hello World!<h1>"
+
+
+# Login method from lectures, edited to fit my needs
+# Takes a POST request in the following format,
+# If username and password is correct, a token is provided
+# NOTE: auth.username is actually the email, just how it has to be done
+@app.route('/api/v1.0/login', methods=['POST'])
+def login():
+    client = MongoClient(CLIENT_CONNECTION, server_api=ServerApi('1'))
+    db = client[DATABASE_CONNECTION]
+    users_collection = db[USERS_COLLECTION]
+
+    auth = request.authorization
+
+    # Check if the request contains the 'Authorization' header
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {
+            'WWW-Authenticate': 'Basic realm="Login Required"'
+        })
+
+    # Query the MongoDB collection for the provided email
+    user = users_collection.find_one({'email': auth.username, 'password': auth.password})
+
+    # Check if the user was found and the password matches
+    if user:
+        # Token is created that contains the username and expiry time
+        token = jwt.encode({
+            'user': auth.username,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        }, app.config['SECRET_KEY'])
+
+        # Returning the JSON obj decoded
+        return jsonify({'token': token})
+    else:
+        return make_response('Invalid credentials', 401, {
+            'WWW-Authenticate': 'Basic realm="Login Required"'
+        })
 
 
 @app.route("/api/v1.0/user/new", methods=["POST"])
